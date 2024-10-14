6175389b142180c82a24c66717d6f4c222ffa516
wedson97 <wedsonsilva025@gmail.com>
2024-10-14 09:52:26 -0300

    Helmet no index

--- src/body/components/posts/post/index.jsx
+++ src/body/components/posts/post/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Helmet } from 'react-helmet'; // Import Helmet
 import './css/style.css'; // Importa o arquivo CSS
 import { useUser } from '../../../../context/UseContext';
 import api from '../../../../api/requisicoes';
@@ -30,36 +31,44 @@ function Post() {
   return (
     <div>
       {posts.map((post) => {
-        const autor = usuarios.find((usuario) => usuario.id === post.autor_id
-      );
-        
+        const autor = usuarios.find((usuario) => usuario.id === post.autor_id);
+
         return (
           <div key={post.id} className="postContainer" style={{ position: 'relative' }}>
-  <Link to={`/post/${post.id}`} className="postLink">
-    <img
-      src={`${api.defaults.baseURL}images/${post.imagem_url}`}
-      alt={`Imagem do ${post.titulo}`}
-      className="image"
-    />
-    <div className="content">
-      <h2 className="title">{post.titulo}</h2>
-    </div>
-  </Link>
-  <div style={{
-    position: 'absolute',
-    bottom: '10px',
-    right: '10px',
-    color:"#b7b7b7",
-    padding: '5px',
-    borderRadius: '5px',
-    display: 'flex',
-    alignItems: 'center'
-  }}>
-    <h6 style={{ marginRight: "10px" }}>{autor ? autor.nome_usuario : 'Autor desconhecido'}</h6>
-    <h6>{new Date(post.criado_em).toLocaleDateString('pt-BR')}</h6>
-  </div>
-</div>
-
+            <Helmet>
+              <title>{post.titulo}</title>
+              <meta property="og:title" content={post.titulo} />
+              <meta property="og:description" content={post.conteudo.substring(0, 100)} />
+              <meta property="og:image" content={`${api.defaults.baseURL}images/${post.imagem_url}`} />
+              <meta property="og:url" content={`http://localhost:5173/post/${post.id}`} />
+              <meta name="twitter:card" content="summary_large_image" />
+            </Helmet>
+            
+            <Link to={`/post/${post.id}`} className="postLink">
+              <img
+                src={`${api.defaults.baseURL}images/${post.imagem_url}`}
+                alt={`Imagem do ${post.titulo}`}
+                className="image"
+              />
+              <div className="content">
+                <h2 className="title">{post.titulo}</h2>
+              </div>
+            </Link>
+            
+            <div style={{
+              position: 'absolute',
+              bottom: '10px',
+              right: '10px',
+              color:"#b7b7b7",
+              padding: '5px',
+              borderRadius: '5px',
+              display: 'flex',
+              alignItems: 'center'
+            }}>
+              <h6 style={{ marginRight: "10px" }}>{autor ? autor.nome_usuario : 'Autor desconhecido'}</h6>
+              <h6>{new Date(post.criado_em).toLocaleDateString('pt-BR')}</h6>
+            </div>
+          </div>
         );
       })}
     </div>
